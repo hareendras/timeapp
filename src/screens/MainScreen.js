1a610a2d5985db8ff99897b1f7c5f80f0b3cb330
Seneviratne <hareendras@lseg.stockex.local>
2017-11-20 13:34:37 +0530

    Go button added

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Autocomplete from "react-native-autocomplete-input";
+import { Constants, Svg, Path } from "expo";
 import _ from "lodash";
 import {
   StyleSheet,
@@ -12,6 +13,7 @@ import {
   ScrollView
 } from "react-native";
 import { connect } from "react-redux";
+import Button from "react-native-button";
 import * as actions from "../actions";
 import bgImage from "../../assets/img/126.42.jpg";
 
@@ -58,49 +60,51 @@ class MainScreen extends Component {
     return (
       <Image source={bgImage} style={styles.imgContainer}>
         <View>
-          <Autocomplete
-            style={styles.textInpTitle}
-            underlineColorAndroid="transparent"
-            autoCapitalize="none"
-            autoCorrect={false}
-            containerStyle={styles.autocompleteContainer}
-            listStyle={{ borderWidth: 0 }}
-            hideResults={hideResults}
-            data={
-              this.props.results &&
-              this.props.results.length > 1 &&
-              this.props.results &&
-              comp(query, this.props.results[0].original_name)
-                ? []
-                : this.props.results
-            }
-            defaultValue={query}
-            onChangeText={text => this.onChangeTextDelayed(text)}
-            placeholder="Type-in a TV show"
-            renderItem={({ original_name, id }) => (
-              <TouchableOpacity
-                onPress={() => {
-                  this.setState({
-                    query: original_name,
-                    hideResults: true
-                  });
-                  this.onPressResult(id);
-                }}
-              >
-                <Text>{original_name}</Text>
-              </TouchableOpacity>
-            )}
-          />
-        </View>
-
-        <TextInput
-          underlineColorAndroid="transparent"
-          style={styles.textInpSeason}
-          placeholder="Seasons #"
-          defaultValue={`${this.props.tvResults.number_of_seasons}`}
-        />
-
-        { /*<TouchableOpacity style={styles.btnStyle}>
+          <View style={styles.firstRow}>
+            <Autocomplete
+              style={styles.textInpTitle}
+              underlineColorAndroid="transparent"
+              autoCapitalize="none"
+              autoCorrect={false}
+              containerStyle={styles.autocompleteContainer}
+              listStyle={{ borderWidth: 0 }}
+              hideResults={hideResults}
+              data={
+                this.props.results &&
+                this.props.results.length > 1 &&
+                this.props.results &&
+                comp(query, this.props.results[0].original_name)
+                  ? []
+                  : this.props.results
+              }
+              defaultValue={query}
+              onChangeText={text => this.onChangeTextDelayed(text)}
+              placeholder="Type-in a TV show"
+              renderItem={({ original_name, id }) => (
+                <TouchableOpacity
+                  onPress={() => {
+                    this.setState({
+                      query: original_name,
+                      hideResults: true
+                    });
+                    this.onPressResult(id);
+                  }}
+                >
+                  <Text>{original_name}</Text>
+                </TouchableOpacity>
+              )}
+            />
+          </View>
+          <View style={styles.secondRow}>
+            <View>
+              <TextInput
+                underlineColorAndroid="transparent"
+                style={styles.textInpSeason}
+                placeholder="Seasons #"
+                defaultValue={`${this.props.tvResults.number_of_seasons}`}
+              />
+            </View>
+            {/*<TouchableOpacity style={styles.btnStyle}>
           <Text>TEST BTN</Text>
         </TouchableOpacity>
         <ScrollView horizontal>
@@ -110,6 +114,31 @@ class MainScreen extends Component {
           <Text>Item 2</Text>
           <Text>Item 3</Text>
         </ScrollView> */}
+
+            <Svg style={styles.goBtn} height="100" width="100">
+              <Svg.Path d="M50,18c-17.6,0-32,14.4-32,32s14.4,32,32,32s32-14.4,32-32S67.6,18,50,18z M50,76c-14.3,0-26-11.7-26-26s11.7-26,26-26   s26,11.7,26,26S64.3,76,50,76z" />
+              <Svg.Path d="M46.2,35.4c-1.1-1.2-3-1.2-4.2-0.1c-1.2,1.1-1.2,3-0.1,4.2l10,10.4l-10,10.4c-1.1,1.2-1.1,3.1,0.1,4.2   c0.6,0.6,1.3,0.8,2.1,0.8c0.8,0,1.6-0.3,2.2-0.9l12-12.5c1.1-1.2,1.1-3,0-4.2L46.2,35.4z" />
+            </Svg>
+
+            <Button
+              containerStyle={{
+                marginTop: 200,
+                padding: 10,
+                height: 70,
+                overflow: "hidden",
+                borderRadius: 100,
+                backgroundColor: "transparent"
+              }}
+              disabledContainerStyle={{ backgroundColor: "transparent" }}
+              style={{ fontSize: 20, color: "transparent" }}
+            >
+              xxxxx
+            </Button>
+          </View>
+          <View>
+            <Text> outer </Text>
+          </View>
+        </View>
       </Image>
     );
   }
@@ -144,7 +173,31 @@ const styles = StyleSheet.create({
   },
   btnStyle: {
     left: 170
-  }
+  },
+  firstRow: {
+    // backgroundColor: "red"
+  },
+  secondRow: {
+    flexDirection: "row"
+    //  backgroundColor: "green"
+  },
+  goBtn: {
+    marginTop: 185,
+    position: "absolute",
+    marginLeft: 200
+  },
+  goBtnContainer: {
+    marginTop: 200,
+    padding: 10,
+    height: 70,
+    overflow: "hidden",
+    borderRadius: 4,
+    backgroundColor: "grey"
+  },
+  goBtndisabledContainerStyle: {
+    backgroundColor: "transparent"
+  },
+  goBtnStyle: {}
 });
 
 //mapping of component properties to state ====>>>>>>
@@ -166,7 +219,7 @@ const styles = StyleSheet.create({
 //and under results piece of state we have results array and tvResults
 //array which are handled by movie_reducer
 
-const mapStateToProps = state => {  
+const mapStateToProps = state => {
   return {
     results: state.results.results,
     tvResults: state.results.tvResults
